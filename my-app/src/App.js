53fedeb1175d796eb95083dac8b8e09d113c3795
Nilesh kumar <kumarnilesh843127@gmail.com>
2024-10-25 16:02:02 +0530

    react js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,3 @@
-// import React from 'react'
-
-
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
-// import Home from './Components/Router/Home';
-// import About from './Components/Router/About';
-// import Contact from './Components/Router/Contact';
-
-// const App = () => {
-//   return (
-//     <>
-
-//     <Router>
-//        <div><Link to='../src/Components/Router/Home.js'>Home </Link></div>
-//        <div><Link to='../src/Components/Router/About.js'>About </Link></div>
-//        <div><Link to='../src/Components/Router/Contact.js'>Contact</Link></div>
-
-//        <Routes>
-//           <Route path="../src/Components/Router/Home.js" element={<Home/>}/>
-//           <Route path="../src/Components/Router/About.js" element={<About/>}/>
-//           <Route path="../src/Components/Router/Contact.js" element={<Contact/>}/>
-//        </Routes>
-//     </Router>
-    
-    
-    
-//     </>
-//   )
-// }
-
-// export default App
-
-
 
 
 
